78defe01aeaf51b546a6733950208ce7d07f0b71
Brinton <brintonfoyreed@gmail.com>
2021-11-08 21:17:04 -0800

    Halfway finished

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,24 +1,52 @@
 function romanNumeral(string) {
-  // type your code here
+  if (typeof num !== "number") {
+    return false;
+  }
+
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+
+
+
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 1");
-  console.log(romanNumeral('I'));
+  console.log(romanNumeral("I"));
 
   console.log("");
 
   console.log("Expecting: 9");
-  console.log(romanNumeral('IX'));
+  console.log(romanNumeral("IX"));
 
   console.log("");
 
   console.log("Expecting: 402");
-  console.log(romanNumeral('CDII'));
+  console.log(romanNumeral("CDII"));
 }
 
 module.exports = romanNumeral;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// Break num down to it's numeric parts (1776 = 1000 + 700 + 70 + 6)
+// And a written explanation of your solution. Use length method to
+// separate first digit, then match with first digit numerals and proceed
+// to the second, and so on and so forth ad nauseum.
+
+// declaire empty string to hold converted number (str).
+// Loop through object's keys:
+// establish a q variable to hold quotient (num / roman[iteration of obj. keys])
+// subtract the q variable 
